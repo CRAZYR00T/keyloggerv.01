ed75c0dacd4496d607791820da5b44ae463ee691
CRAZYR00T <crazyflashroot@gmail.com>
2021-04-06 16:49:09 +0100

    Adding files

---
+++ config.py
@@ -0,0 +1,11 @@
+# ONLY WORKS WITH OUTLOOK
+# IF YOU WANNA CHANGE FOR A GMAIL ACCOUNT
+# NEED TO GO ON (file007.py) AND FIND S.SMPTPLIB.SMTP() AND CHANGE FOR "smtp.gmail.com"
+
+# fromAddr = Your e-email
+# fromPswd = Your password
+
+
+
+fromAddr = 'default e-mail'
+fromPswd = 'default password'
